32bc7c0b163c9ea0cd0ad11f66d193a54e67dcf3
Veena-123-dotcom <lpriyanka489@gmail.com>
2024-11-29 20:14:20 +0530

    new msge

--- src/main/java/ee/affdvs.java
+++ src/main/java/ee/affdvs.java
@@ -5,6 +5,7 @@ public class affdvs
 	public static void main(String []args)
 	{
 		System.out.println("addd new file");
+		System.out.println("add");
 	}
 
 }
---
+++ src/main/java/ee/www.java
@@ -0,0 +1,9 @@
+package ee;
+
+public class www 
+{
+	public static void main(String[] args) {
+		System.out.println("java main class");
+	}
+
+}
